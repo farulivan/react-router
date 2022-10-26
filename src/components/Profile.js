de7e589da2d7faffbb2b7933937d7468a13cc7c3
Farul Ivan <farulivan@gmail.com>
2022-10-26 12:40:01 +0700

    add Redirect to Profile for user that not logged in

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Route, Link, useRouteMatch } from "react-router-dom";
+import { Route, Link, useRouteMatch, Redirect } from "react-router-dom";
 import { selectCurrentUser, selectIsLoggedIn } from "../features/session/sessionSlice";
 import EditProfileForm from "./EditProfileForm";
 
@@ -10,7 +10,9 @@ export default function Profile () {
 
   const { path, url } = useRouteMatch()
   
-  // use loggedIn to return a Redirect
+  if(!loggedIn){
+    return <Redirect to={'/sign-up'} />
+  }
   
   return (
     <main>
